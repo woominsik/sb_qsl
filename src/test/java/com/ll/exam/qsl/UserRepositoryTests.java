f24a2f84a952a173f748eb99ed349cadea2e49be
woominsik <9737638@naver.com>
2022-08-29 14:20:02 +0900

    테스트 클래스에 Transactional 붙이기

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -7,12 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+// 이렇게 클래스 @Transactional를 붙이면, 클래스의 각 테스트케이스에 전부 @Transactional 붙은 것과 동일
+// @Test + @Transactional 조합은 자동으로 롤백을 유발시킨다.
+@Transactional
 @ActiveProfiles("test") // 테스트 모드 활성화
 class UserRepositoryTests {
     @Autowired
