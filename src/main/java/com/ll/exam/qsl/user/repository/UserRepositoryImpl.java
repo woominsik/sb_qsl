b01d2d54897c3ca9871545a265ad5948f6d3638d
woominsik <9737638@naver.com>
2022-08-30 14:06:53 +0900

    paging 검색 구현

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -1,12 +1,19 @@
 package com.ll.exam.qsl.user.repository;
 
 import com.ll.exam.qsl.user.entity.SiteUser;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
+import java.util.function.LongSupplier;
 
 import static com.ll.exam.qsl.user.entity.QSiteUser.siteUser;
 
@@ -64,6 +71,25 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 
     @Override
     public Page<SiteUser> searchQsl(String kw, Pageable pageable) {
-        return null;
+        JPAQuery<SiteUser> usersQuery = jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .where(
+                        siteUser.username.contains(kw)
+                                .or(siteUser.email.contains(kw))
+                )
+                .offset(pageable.getOffset()) // 몇개를 건너 띄어야 하는지 LIMIT {1}, ?
+                .limit(pageable.getPageSize()); // 한페이지에 몇개가 보여야 하는지 LIMIT ?, {1}
+
+        for (Sort.Order o : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(siteUser.getType(), siteUser.getMetadata());
+            usersQuery.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+        }
+
+        List<SiteUser> users = usersQuery.fetch();
+
+        LongSupplier totalSupplier = () -> 2;
+
+        return PageableExecutionUtils.getPage(users, pageable, totalSupplier);
     }
 }
--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -137,34 +137,62 @@ class UserRepositoryTests {
     }
 
     @Test
-    @DisplayName("검색, Page 리턴")
+    @DisplayName("검색, Page 리턴, id ASC, pageSize=1, page=0")
     void t8() {
-        int itemsInAPage = 1; // 한 페이지에 보여줄 아이템 개수
+        long totalCount = userRepository.count();
+        int pageSize = 1; // 한 페이지에 보여줄 아이템 개수
+        int totalPages = (int)Math.ceil(totalCount / (double)pageSize);
+        int page = 1;
+        String kw = "user";
+
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.asc("id"));
-        Pageable pageable = PageRequest.of(1, itemsInAPage, Sort.by(sorts)); // 한 페이지에 10까지 가능
-        Page<SiteUser> users = userRepository.searchQsl("user", pageable);
-        // 검색어 : user1
-        // 한 페이지에 나올 수 있는 아이템 수 : 1개
-        // 현재 페이지 : 1
-        // 정렬 : id 역순
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sorts)); // 한 페이지에 10까지 가능
+        Page<SiteUser> usersPage = userRepository.searchQsl(kw, pageable);
 
-        // 내용 가져오는 SQL
-        /*
-        SELECT site_user.*
-        FROM site_user
-        WHERE site_user.username LIKE '%user%'
-        OR site_user.email LIKE '%user%'
-        ORDER BY site_user.id ASC
-        LIMIT 1, 1
-         */
+        assertThat(usersPage.getTotalPages()).isEqualTo(totalPages);
+        assertThat(usersPage.getNumber()).isEqualTo(page);
+        assertThat(usersPage.getSize()).isEqualTo(pageSize);
 
-        // 전체 개수 계산하는 SQL
-        /*
-        SELECT COUNT(*)
-        FROM site_user
-        WHERE site_user.username LIKE '%user%'
-        OR site_user.email LIKE '%user%'
-         */
+        List<SiteUser> users = usersPage.get().toList();
+
+        assertThat(users.size()).isEqualTo(pageSize);
+
+        SiteUser u = users.get(0);
+
+        assertThat(u.getId()).isEqualTo(2L);
+        assertThat(u.getUsername()).isEqualTo("user2");
+        assertThat(u.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("검색, Page 리턴, id DESC, pageSize=1, page=0")
+    void t9() {
+        long totalCount = userRepository.count();
+        int pageSize = 1; // 한 페이지에 보여줄 아이템 개수
+        int totalPages = (int)Math.ceil(totalCount / (double)pageSize);
+        int page = 1;
+        String kw = "user";
+
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("id"));
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sorts)); // 한 페이지에 10까지 가능
+        Page<SiteUser> usersPage = userRepository.searchQsl(kw, pageable);
+
+        assertThat(usersPage.getTotalPages()).isEqualTo(totalPages);
+        assertThat(usersPage.getNumber()).isEqualTo(page);
+        assertThat(usersPage.getSize()).isEqualTo(pageSize);
+
+        List<SiteUser> users = usersPage.get().toList();
+
+        assertThat(users.size()).isEqualTo(pageSize);
+
+        SiteUser u = users.get(0);
+
+        assertThat(u.getId()).isEqualTo(1L);
+        assertThat(u.getUsername()).isEqualTo("user1");
+        assertThat(u.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
     }
 }
